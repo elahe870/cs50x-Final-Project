f951fe197215a7734d7218764eda6500a887faeb
elahe870 <es.haghighat95@gmail.com>
2025-05-05 11:18:10 +0000

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required
+#from helpers import apology, login_required
 
 import datetime
 
@@ -21,3 +21,28 @@ Session(app)
 
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///GMP.db")
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    # Get the current date
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+    # Get the user's ID from the session
+    user_id = session["user_id"]
+
+    # Query the database for 
+    /*stocks = db.execute("SELECT * FROM stocks WHERE user_id = ?", user_id)
+*/
+    # Render the index.html template with the stocks data and current date
+    return render_template("index.html")
+
+
+@app.route("/logout")
+def logout():
+    "log user out"
+    #forget any user_id
+    session.clear()
+    #redirect user to login form
+    return redirect("/login")
