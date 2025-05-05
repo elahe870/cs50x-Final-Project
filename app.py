bd2f14e8048e182b5a021c40c84652a6a23661c1
elahe870 <es.haghighat95@gmail.com>
2025-05-05 11:28:16 +0000

    third commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-#from helpers import apology, login_required
+from helpers import apology, login_required
 
 import datetime
 
@@ -33,8 +33,8 @@ def index():
     user_id = session["user_id"]
 
     # Query the database for 
-    /*stocks = db.execute("SELECT * FROM stocks WHERE user_id = ?", user_id)
-*/
+    #stocks = db.execute("SELECT * FROM stocks WHERE user_id = ?", user_id)
+
     # Render the index.html template with the stocks data and current date
     return render_template("index.html")
 
