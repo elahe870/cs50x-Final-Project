789522d62fefb80e9be47d8cd0c4684bd3a92b4d
elahe870 <es.haghighat95@gmail.com>
2025-05-06 08:09:37 +0000

    fourth commit

--- app.py
+++ app.py
@@ -25,24 +25,84 @@ db = SQL("sqlite:///GMP.db")
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
-    # Get the current date
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
-    # Get the user's ID from the session
-    user_id = session["user_id"]
-
-    # Query the database for 
-    #stocks = db.execute("SELECT * FROM stocks WHERE user_id = ?", user_id)
-
-    # Render the index.html template with the stocks data and current date
+    if not session.get("user_id"):
+        return redirect("/login")
     return render_template("index.html")
 
 
 @app.route("/logout")
 def logout():
+    #adapted from CS50's Finance
     "log user out"
     #forget any user_id
     session.clear()
     #redirect user to login form
     return redirect("/login")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    #adapted from CS50's Finance
+    """Log user in"""
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+    
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    #adapted from my cs50x problem set Finance
+    """Register user"""
+    if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 400)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 400)
+        elif not request.form.get("confirmation"):
+            return apology("No confirmation", 400)
+        elif request.form.get("password") != request.form.get("confirmation"):
+            return apology("password and confirmation not match", 400)
+        # search for username
+        user = request.form.get("username")
+        exist = db.execute("SELECT * FROM users WHERE username = ?", user)
+        if len(exist) == 1:
+            return apology("Username is not valid! choose another username")
+
+        # append database
+        hashed = generate_password_hash(request.form.get("password"))
+
+        db.execute(
+            "INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), hashed)
+    elif request.method == "GET":
+        return render_template("register.html")
+
+    return render_template("login.html", message="Registration successful!")
+  
